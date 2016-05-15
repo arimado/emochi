118375dea499508e6f4d1bd0d7ae1013851d960d
Jeremy Arimado <jarimado@gmail.com>
2016-05-15 14:29:27 +1000

    init 2

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function (req, res) {
-    res.send('hello world');
-}); 
+    res.send('hello worldd');
+});
 
 http.listen(3000, function () {
     console.log('listening on 3000');
