df4902e045918fa29a5ce724b0a0d96f1063f1c2
Jeremy Arimado <jarimado@gmail.com>
2016-05-15 15:36:50 +1000

    added hbs, path, public dir

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+var express = require('express'); 
 var app = require('express')();
+var path = require('path'); //
 var http = require('http').Server(app);
 
+// SETUP VIEW ENGINE
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 app.get('/', function (req, res) {
-    res.send('hello worldd');
+    res.render('index');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 http.listen(3000, function () {
     console.log('listening on 3000');
