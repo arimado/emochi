6027b42b842a2137ffa2467cbffa1ea49728648e
Jeremy Arimado <jarimado@gmail.com>
2016-05-15 16:15:26 +1000

    broadcasting and appending messages

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var express = require('express'); 
+var express = require('express');
 var app = require('express')();
 var path = require('path'); //
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 // SETUP VIEW ENGINE
 app.set('views', path.join(__dirname, 'views'));
@@ -12,6 +13,20 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function (socket) {
+    console.log('a user connected!');
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg)
+    });
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+});
 
 http.listen(3000, function () {
     console.log('listening on 3000');
---
+++ public/js/client.js
@@ -0,0 +1,10 @@
+var socket = io();
+$('form').submit(function(){
+    var chatMsg = $('#m').val();
+    socket.emit('chat message', chatMsg);
+     $('#m').val('');
+    return false; // why this
+})
+socket.on('chat message', function (msg) {
+    $('#messages').append($('<li>').text(msg)); 
+});
